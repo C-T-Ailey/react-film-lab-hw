23cbba73e568d4bedcc8413ad52cbe8243d5fc61
cailey90 <chris.ailey90@gmail.com>
2022-07-11 12:27:44 +0100

    refactored Fave.js into functional component

--- src/Fave.js
+++ src/Fave.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export default class Fave extends Component {
+export default function Fave(props) {
 
 //   constructor(props) {
 //     super(props)
@@ -10,19 +10,19 @@ export default class Fave extends Component {
 //     }
 //   }
 
-  handleClick = (e) => {
-      this.props.onFaveToggle()
+  const handleClick = (e) => {
+      props.onFaveToggle()
       e.stopPropagation()
   }
 
-  render() {
-    const faveClassCheck = (this.props.isFave) ? 'remove_from_queue' : 'add_to_queue'
+
+  const faveClassCheck = (props.isFave) ? 'remove_from_queue' : 'add_to_queue'
     // console.log(this.props.isFave)
-    return (
+  return (
     //   <div className='film-row-fave' onClick={this.handleClick}>
-      <div className={faveClassCheck + " film-row-fave"} onClick={this.handleClick}>
-        <p className='material-icons'>{faveClassCheck}</p>
-      </div>
-    )
-  }
+    <div className={faveClassCheck + " film-row-fave"} onClick={handleClick}>
+      <p className='material-icons'>{faveClassCheck}</p>
+    </div>
+  )
 }
+
